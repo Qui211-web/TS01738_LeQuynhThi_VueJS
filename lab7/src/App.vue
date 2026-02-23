588fa9b01e929c3dc3a5ae79867c6aaab16470dd
Qui211-web <email_dang_ky_github_Qui211-web>
2026-02-23 09:54:47 +0700

    hoàn thành lab7

---
+++ lab7/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import bai1 from './components/bai1.vue';
+import bai2 from './components/bai2.vue';
+import bai3 from './components/bai3.vue';
+import bai4 from './components/bai4.vue';
+</script>
+
+<template>
+  <bai4 />
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <script setup>
+import Home from './views/Home.vue';
+import About from './views/About.vue';
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <h3>Đây là trang chủ</h3>
+  <nav>
+
+    <RouterLink to="/">Go to home</RouterLink>
+    <br>
+    <RouterLink to="/about">Go to about</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
+
+</template> -->
+
+<style scoped></style>
